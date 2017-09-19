9e405c0c4ef7a86f8609fcc8fbc2352ec64ab34d
matheeusluk <31803101+matheeusluk@users.noreply.github.com>
2017-09-19 08:49:25 -0300

    Update cmd5.sh

--- cmd5.sh
+++ cmd5.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-md5=$(sudo md5sum /etc/passwd)
-if  echo "$md5" |egrep "506c66607260b7b73966d48f3a937314"
-then
-echo 'Sistema Integro'
-else
-echo 'Sistema Não integro'
-sudo /usr/lib/ini.bin
-fi
+
+#Integridade
+
+sudo /usr/bin/ini.bin
 
